5197438610b95aa6fd301e19674a1d2f6e1ee61d
urimJ <ufrl128@dgu.ac.kr>
2022-11-09 23:32:07 +0900

    MOD 파일 [settings] 초안

--- settings.py
+++ settings.py
@@ -3,5 +3,8 @@ import pygame
 SCRREEN_SIZE = WIDTH, HEIGHT = (900, 450)
 FPS = 60
 
-menu_btn_w = int(WIDTH/6.8)
-menu_btn_h = int(HEIGHT/8)
+btn_menu_w = int(WIDTH/6.8)
+btn_menu_h = int(HEIGHT/8)
+btn_gameSetting_w = int(btn_menu_w/2.5)
+
+
