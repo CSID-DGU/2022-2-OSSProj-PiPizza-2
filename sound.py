b73c5d2c4b27867d4c0676e3220812ce5b822ca0
urimJ <ufrl128@dgu.ac.kr>
2022-11-13 23:02:41 +0900

    ADD 사운드 클래스

---
+++ sound.py
@@ -0,0 +1,10 @@
+class Sound:
+    def __init__(self):
+        pass
+    
+    # 메뉴화면 환경설정 함수
+    def set_soundOn(self):
+        print("소리 켜기 버튼 눌림")
+    # 메뉴화면 환경설정 함수
+    def set_soundOff(self):
+        print("소리 끄기 버튼 눌림")
