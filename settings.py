4130ec80611bc3b4434009764c13cd76ad12f471
holy0 <96487453+holy0@users.noreply.github.com>
2022-11-19 21:48:48 +0900

    MOD 변수명 변경

---
+++ settings.py
@@ -0,0 +1,9 @@
+import pygame
+
+SCRREEN_SIZE = WIDTH, HEIGHT = (900, 450)
+FPS = 60
+
+# 메뉴화면 버튼 크기
+btn_menu_w = int(WIDTH/6.8)
+btn_menu_h = int(HEIGHT/8)
+btn_gameSetting_w = int(btn_menu_w/2)
--- stage1.py
+++ stage1.py
@@ -1,121 +1,119 @@
 import pygame, sys
 import os
 import random
-from settings1 import *
+from settings import *
 
 pygame.init()
 
 #화면 타이틀 설정
-pygame.display.set_caption("PiPizza")
+pygame.display.set_caption("배달의 달인")
 
 isClear = False
 start_ticks = pygame.time.get_ticks() # 현재 tick 을 받아옴
-total_time = 20 #총 시간
+total_time = 40 #총 시간
 elapsed_time = (pygame.time.get_ticks() - start_ticks)/1000
 
-SCREEN_HEIGHT = 450
-SCREEN_WIDTH = 900
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-RUNNING = [pygame.image.load(os.path.join("Images/Dino", "Bike1.png")),
-           pygame.image.load(os.path.join("Images/Dino", "Bike2.png"))]
-JUMPING = pygame.image.load(os.path.join("Images/Dino", "Bike2.png"))
-DUCKING = [pygame.image.load(os.path.join("Images/Dino", "BikeDuck1.png")),
-           pygame.image.load(os.path.join("Images/Dino", "BikeDuck1.png"))]
+RUNNING = [pygame.image.load(os.path.join("images/sprites", "Bike1.png")),
+           pygame.image.load(os.path.join("images/sprites", "Bike2.png"))]
+JUMPING = pygame.image.load(os.path.join("images/sprites", "Bike2.png"))
+DUCKING = [pygame.image.load(os.path.join("images/sprites", "BikeDuck1.png")),
+           pygame.image.load(os.path.join("images/sprites", "BikeDuck1.png"))]
 
 
 
-SMALL_CACTUS = [pygame.image.load(os.path.join("Images/Cactus", "Traffic1.png")),
-                pygame.image.load(os.path.join("Images/Cactus", "Traffic3.png")),
-                pygame.image.load(os.path.join("Images/Cactus", "Traffic4.png"))]
-LARGE_CACTUS = [pygame.image.load(os.path.join("Images/Cactus", "RoadBlock.png")),
-                pygame.image.load(os.path.join("Images/Cactus", "TrafficCone.png")),
-                pygame.image.load(os.path.join("Images/Cactus", "TrafficCone2.png"))]
+Traffic_Light = [pygame.image.load(os.path.join("images/obstacles", "Traffic1.png")),
+                pygame.image.load(os.path.join("images/obstacles", "Traffic3.png")),
+                pygame.image.load(os.path.join("images/obstacles", "Traffic4.png"))]
+Traffic_Cone = [pygame.image.load(os.path.join("images/obstacles", "RoadBlock.png")),
+                pygame.image.load(os.path.join("images/obstacles", "TrafficCone.png")),
+                pygame.image.load(os.path.join("images/obstacles", "TrafficCone2.png"))]
 
-DUST = [pygame.image.load(os.path.join("Images/Bird", "Dust1.png")),
-        pygame.image.load(os.path.join("Images/Bird", "Dust2.png"))]
+DUST = [pygame.image.load(os.path.join("images/obstacles", "Dust1.png")),
+        pygame.image.load(os.path.join("images/obstacles", "Dust2.png"))]
 
-CLOUD = pygame.image.load(os.path.join("Images/Other", "Cloud.png"))
+CLOUD = pygame.image.load(os.path.join("images/obstacles", "Cloud.png"))
 
-BG = pygame.image.load(os.path.join("Images/Other", "Track.png"))
+BG = pygame.image.load(os.path.join("images/obstacles", "Track.png"))
 
 
-class Dinosaur:
-    X_POS = SCREEN_WIDTH/5 #90
-    Y_POS = SCREEN_HEIGHT*0.66 #310
-    Y_POS_DUCK = SCREEN_HEIGHT*0.75
-    JUMP_VEL = SCREEN_HEIGHT/52.5
+class Bike:
+    X_POS = WIDTH/5 #90
+    Y_POS = HEIGHT*0.66 #310
+    Y_POS_DUCK = HEIGHT*0.75
+    JUMP_VEL = HEIGHT/52.5
 
     def __init__(self):
         self.duck_img = DUCKING
         self.run_img = RUNNING
         self.jump_img = JUMPING
 
-        self.dino_duck = False
-        self.dino_run = True
-        self.dino_jump = False
+        self.bike_duck = False
+        self.bike_run = True
+        self.bike_jump = False
 
         self.step_index = 0
         self.jump_vel = self.JUMP_VEL
         self.image = self.run_img[0]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS
+        self.bike_rect = self.image.get_rect()
+        self.bike_rect.x = self.X_POS
+        self.bike_rect.y = self.Y_POS
 
     def update(self, userInput):
-        if self.dino_duck:
+        if self.bike_duck:
             self.duck()
-        if self.dino_run:
+        if self.bike_run:
             self.run()
-        if self.dino_jump:
+        if self.bike_jump:
             self.jump()
 
         if self.step_index >= 10:
             self.step_index = 0
 
-        if userInput[pygame.K_UP] and not self.dino_jump:
-            self.dino_duck = False
-            self.dino_run = False
-            self.dino_jump = True
-        elif userInput[pygame.K_DOWN] and not self.dino_jump:
-            self.dino_duck = True
-            self.dino_run = False
-            self.dino_jump = False
-        elif not (self.dino_jump or userInput[pygame.K_DOWN]):
-            self.dino_duck = False
-            self.dino_run = True
-            self.dino_jump = False
+        if userInput[pygame.K_UP] and not self.bike_jump:
+            self.bike_duck = False
+            self.bike_run = False
+            self.bike_jump = True
+        elif userInput[pygame.K_DOWN] and not self.bike_jump:
+            self.bike_duck = True
+            self.bike_run = False
+            self.bike_jump = False
+        elif not (self.bike_jump or userInput[pygame.K_DOWN]):
+            self.bike_duck = False
+            self.bike_run = True
+            self.bike_jump = False
 
     def duck(self):
         self.image = self.duck_img[self.step_index // 5]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS_DUCK
+        self.bike_rect = self.image.get_rect()
+        self.bike_rect.x = self.X_POS
+        self.bike_rect.y = self.Y_POS_DUCK
         self.step_index += 1
 
     def run(self):
         self.image = self.run_img[self.step_index // 5]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS
+        self.bike_rect = self.image.get_rect()
+        self.bike_rect.x = self.X_POS
+        self.bike_rect.y = self.Y_POS
         self.step_index += 1
 
     def jump(self):
         self.image = self.jump_img
-        if self.dino_jump:
-            self.dino_rect.y -= self.jump_vel * 4
+        if self.bike_jump:
+            self.bike_rect.y -= self.jump_vel * 4
             self.jump_vel -= 0.8
         if self.jump_vel < - self.JUMP_VEL:
-            self.dino_jump = False
+            self.bike_jump = False
             self.jump_vel = self.JUMP_VEL
 
     def draw(self, SCREEN):
-        SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+        SCREEN.blit(self.image, (self.bike_rect.x, self.bike_rect.y))
 
 
 class Cloud:
     def __init__(self):
-        self.x = SCREEN_WIDTH + random.randint(800, 1000)
+        self.x = WIDTH + random.randint(800, 1000)
         self.y = random.randint(50, 100)
         self.image = CLOUD
         self.width = self.image.get_width()
@@ -123,7 +121,7 @@ class Cloud:
     def update(self):
         self.x -= game_speed
         if self.x < -self.width:
-            self.x = SCREEN_WIDTH + random.randint(2500, 3000)
+            self.x = WIDTH + random.randint(2500, 3000)
             self.y = random.randint(50, 100)
 
     def draw(self, SCREEN):
@@ -135,7 +133,7 @@ class Obstacle:
         self.image = image
         self.type = type
         self.rect = self.image[self.type].get_rect()
-        self.rect.x = SCREEN_WIDTH
+        self.rect.x = WIDTH
 
     def update(self):
         self.rect.x -= game_speed
@@ -146,25 +144,25 @@ class Obstacle:
         SCREEN.blit(self.image[self.type], self.rect)
 
 
-class SmallCactus(Obstacle):
+class TrafficLight(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
-        self.rect.y = SCREEN_HEIGHT*2/3
+        self.rect.y = HEIGHT*2/3
 
 
-class LargeCactus(Obstacle):
+class TrafficCone(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
-        self.rect.y = SCREEN_HEIGHT*0.78
+        self.rect.y = HEIGHT*0.78
 
  
 class Dust(Obstacle):
     def __init__(self, image):
         self.type = 0
         super().__init__(image, self.type)
-        self.rect.y = SCREEN_HEIGHT*0.50
+        self.rect.y = HEIGHT*0.50
         self.index = 0
 
     def draw(self, SCREEN):
@@ -178,9 +176,9 @@ def main():
     global game_speed, x_pos_bg, y_pos_bg, points, obstacles
     run = True
     clock = pygame.time.Clock()
-    player = Dinosaur()
+    player = Bike()
     cloud = Cloud()
-    game_speed = 22.5
+    game_speed = 20
     x_pos_bg = 0
     y_pos_bg = 380
     points = 0
@@ -223,9 +221,9 @@ def main():
 
         if len(obstacles) == 0:
             if random.randint(0, 2) == 0:
-                obstacles.append(SmallCactus(SMALL_CACTUS))
+                obstacles.append(TrafficLight(Traffic_Light))
             elif random.randint(0, 2) == 1:
-                obstacles.append(LargeCactus(LARGE_CACTUS))
+                obstacles.append(TrafficCone(Traffic_Cone))
             elif random.randint(0, 2) == 2:
                 obstacles.append(Dust(DUST))
        
@@ -233,7 +231,7 @@ def main():
         for obstacle in obstacles:
             obstacle.draw(SCREEN)
             obstacle.update()
-            if player.dino_rect.colliderect(obstacle.rect):
+            if player.bike_rect.colliderect(obstacle.rect):
                 pygame.time.delay(2000)
                 elapsed_time = 0
                 death_count += 1
@@ -251,7 +249,7 @@ def main():
             print("Game Clear")
             timer = font.render(str(int(total_time-elapsed_time)),True,(0,0,0))
             timerRect = timer.get_rect()
-            timerRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
+            timerRect.center = (WIDTH // 2, HEIGHT // 2 + 50)
             SCREEN.blit(timer, timerRect)
             run=False # 다이얼로그로 넘어가야 함
 
@@ -286,9 +284,9 @@ def menu(death_count):
             #SCREEN.blit(score, scoreRect)
             
         textRect = text.get_rect()
-        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        textRect.center = (WIDTH // 2, HEIGHT // 2)
         SCREEN.blit(text, textRect)
-        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
+        SCREEN.blit(RUNNING[0], (WIDTH // 2 - 20, HEIGHT // 2 - 140))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
