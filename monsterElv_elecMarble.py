be98710ca10ffb9542f9c619af32e08173881464
urimJ <ufrl128@dgu.ac.kr>
2022-11-26 21:06:09 +0900

    ADD 보스 스킬 세 가지

---
+++ monsterElv_elecMarble.py
@@ -0,0 +1,6 @@
+import pygame
+from pygame.locals import *
+
+class monsterElv_elecMarble:
+    def __init__(self):
+        pass
---
+++ monsterElv_elecRain.py
@@ -0,0 +1,6 @@
+import pygame
+from pygame.locals import *
+
+class MonsterElv_elecRain:
+    def __init__(self):
+        pass
