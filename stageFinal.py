a0163f8539563ddef8a59ca30807f414579fa626
urimJ <ufrl128@dgu.ac.kr>
2022-11-26 20:58:18 +0900

    ADD 최종 스테이지

--- stageFinal.py
+++ stageFinal.py
@@ -1,3 +1,27 @@
-class StageFinal:
-    def __init__(self):
+import random
+import pygame
+
+from settings import *
+from level import *
+from monsterElv import *
+from monsterElv_ElecRain import *
+from run import *
+
+class StageFinal(MonsterElv):
+    def __init__(self, pos, MONSTER_SIZE, groups, sprites_obstacles):
+        self.image = pygame.image.load()
+        self.image = pygame.transform.scale(self.iamge, MONSTER_SIZE)
+
+        super(StageFinal, self).__init__(pos, MONSTER_SIZE, groups, sprites_obstacles)
+        # self.speed = 6
+
+        # sound
+
+        self.toward = -1
+
+        self.attackBox = pygame.Rect()
+
+    def lose(self):
+        pass
+    def win(self):
         pass
