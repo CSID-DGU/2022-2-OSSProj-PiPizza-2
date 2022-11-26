d30eb18e1e8f77b1eb76db2c8d0f4fa8fb8d0d82
urimJ <ufrl128@dgu.ac.kr>
2022-11-26 20:59:34 +0900

    MOD 게임 단계 관리

--- level.py
+++ level.py
@@ -10,8 +10,20 @@ class Level:
         # sprite groups 스프라이트 그리(draw)고 업데이트하기
         self.all_sprites = pygame.sprite.Group()
 
+        self.isClear = False
+
     def run(self, df):
         self.display_surface.fill('black')          # 이전 프레임을 가린다
         self.all_sprites.draw(self.display_surface) # 디스플레이 서피스 위에 그린다
         self.all_sprites.update()                   # 스프라이트를 업데이트한다
         # print('게임 실행')
+
+    def monster_create(self, game_state):
+        if game_state == 'level1': # monster1 생성 후 바꿔야함
+            pass
+
+        if game_state == 'level2':
+            pass
+
+        if game_state == 'level3': # boss 생성 후 바꿔야함
+            return stageFinal(DEVIL_COOR_ini, DEVIL_SIZE, [self.visible_sprites], self.obstacle_sprites)
