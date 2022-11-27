42f042e0ea9ba35d629188d30feadb12267b314b
urimJ <ufrl128@dgu.ac.kr>
2022-11-27 12:28:29 +0900

    MOD 몬스터생성 함수, 게임스테이트 함수

--- level.py
+++ level.py
@@ -13,7 +13,16 @@ class Level:
         # sprite groups 스프라이트 그리(draw)고 업데이트하기
         self.all_sprites = pygame.sprite.Group()
 
-        self.isClear = False
+        self.isOneClear = False
+        self.isTwoClear = False
+        self.isFinalClear = False
+        
+        self.sceneNum = 2
+        self.gameState = GAME_STATES[self.sceneNum]
+        self.stateChanging = False
+        self.canChangeState = True
+
+        self.create_map()
 
     def create_map(self):
         # player 생성
@@ -30,11 +39,16 @@ class Level:
         # print('게임 실행')
 
     def monster_create(self, game_state):
-        if game_state == 'level1': # monster1 생성 후 바꿔야함
-            pass
-
-        if game_state == 'level2':
-            pass
-
         if game_state == 'level3': # boss 생성 후 바꿔야함
-            return stageFinal(MonsterElv_COOR_ini, MONSTER_SIZE, [self.visible_sprites], self.obstacle_sprites)
+            return StageFinal(MonsterElv_COOR_ini, MONSTER_SIZE, [self.visible_sprites], self.obstacle_sprites)
+
+    def scene_manager(self):
+        
+        # if self.isOneClear or self.isTwoClear or self.isFinalClear:
+        #     # sceneNum 증가시켜서 gameState 바꾸기
+        #     if self.sceneNum < len(GAME_STATES) -1:
+        #         self.sceneNum += 1
+
+            self.gameState = GAME_STATES[self.sceneNum]
+
+            self.scene = Scene(self.player, self.monster, self.scene_num, self.game_state, self.visible_sprites)
