5061b7a21643018b91817dbe0f7dff0c4ce58dd6
holy0 <96487453+holy0@users.noreply.github.com>
2022-12-10 05:25:16 +0900

    MOD pausing 변수, 스테이지 clear 변경

--- stage1.py
+++ stage1.py
@@ -15,6 +15,7 @@ fullscreen = False
 
 global isClear
 isClear = False
+paused = False
 start_ticks = pygame.time.get_ticks()  # 현재 tick 을 받아옴
 
 total_time = 30  # 총 시간
@@ -38,24 +39,13 @@ def main():
     obstacles = []
     death_count = 0
 
-    # def score():
-    #     global points, game_speed
-    #     points += 1
-    #     if points % 100 == 0:
-    #         game_speed += 1
-
-    #     text = font.render("Points: " + str(points), True, (0, 0, 0))
-    #     textRect = text.get_rect()
-    #     textRect.center = (1000, 40)
-    #     SCREEN.blit(text, textRect)
-
     def background():
         global x_pos_bg, y_pos_bg
         image_width = BG.get_width()
-        SCREEN.blit(BG, (x_pos_bg, y_pos_bg))
-        SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
+        SCREEN_SIZE.blit(BG, (x_pos_bg, y_pos_bg))
+        SCREEN_SIZE.blit(BG, (image_width + x_pos_bg, y_pos_bg))
         if x_pos_bg <= -image_width:
-            SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
+            SCREEN_SIZE.blit(BG, (image_width + x_pos_bg, y_pos_bg))
             x_pos_bg = 0
         x_pos_bg -= game_speed
 
@@ -63,12 +53,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            
+            if event.key == pygame.K_ESCAPE:
+                paused = not paused
+                paused = pausing()
 
-        #SCREEN.fill((247, 155 , 96))
-        SCREEN.fill((255, 255 , 255))        
+        SCREEN_SIZE.fill((255, 255 , 255))        
         userInput = pygame.key.get_pressed()
 
-        player.draw(SCREEN)
+        player.draw(SCREEN_SIZE)
         player.update(userInput)
 
         if len(obstacles) == 0:
@@ -81,35 +74,35 @@ def main():
 
 
         for obstacle in obstacles:
-            obstacle.draw(SCREEN)
+            obstacle.draw(SCREEN_SIZE)
             obstacle.update()
             if player.bike_rect.colliderect(obstacle.rect):
                 pygame.time.delay(2000)
                 elapsed_time = 0
                 death_count += 1
-                stageTwo(death_count)
+                stageOne(death_count)
    
         
         #시간
         elapsed_time = (pygame.time.get_ticks() - start_ticks)/1000
         timer = font.render("TIMER: "+str(int(elapsed_time)),True,(0,0,0))
-        SCREEN.blit(timer,(10,10))
+        SCREEN_SIZE.blit(timer,(10,10))
 
         if total_time - elapsed_time <= 0:
             print("Game Clear")
             timer = font.render(str(int(total_time-elapsed_time)),True,(0,0,0))
             timerRect = timer.get_rect()
-            timerRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
-            SCREEN.blit(timer, timerRect)
+            timerRect.center = (WIDTH // 2, HEIGHT // 2 + 50)
+            SCREEN_SIZE.blit(timer, timerRect)
             death_count = -1
             run=False
-            stageTwo(death_count)
+            stageOne(death_count)
             #run=False # 다이얼로그로 넘어가야 함
 
 
         background()
 
-        cloud.draw(SCREEN)
+        cloud.draw(SCREEN_SIZE)
         cloud.update()
 
        # score()
@@ -125,13 +118,13 @@ def clear(self):  # True를 반환하면 다시 시작
 def fail(self):
     pass
 
-def stageTwo(death_count):
+def stageOne(death_count):
     global points
     run = True
     isClear=False
     while run:
         fullscreen = False
-        SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+        SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
         SCREEN.fill((255, 255, 255))
         font = pygame.font.Font('freesansbold.ttf', 30)
 
@@ -150,13 +143,13 @@ def stageTwo(death_count):
             #SCREEN.blit(score, scoreRect)
         #클리어
         elif death_count < 0:
-            text=font.render("Stage 2 Clear!", True, (0, 0, 0))
+            text=font.render("Stage 1 Clear!", True, (0, 0, 0))
             isClear=True
             #stage 3로 넘어가는 코드
         textRect = text.get_rect()
-        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        textRect.center = (WIDTH // 2, HEIGHT // 2)
         SCREEN.blit(text, textRect)
-        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
+        SCREEN.blit(RUNNING[0], (WIDTH // 2 - 20, HEIGHT // 2 - 140))
         pygame.display.update()
         
         for event in pygame.event.get():
