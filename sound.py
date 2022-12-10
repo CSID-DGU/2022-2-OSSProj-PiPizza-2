a214892db7b42ab129cea99e0a1983d1640dce26
ottffss1005 <ottffss1005@naver.com>
2022-12-10 19:33:06 +0900

    MOD 사운드

--- sound.py
+++ sound.py
@@ -10,10 +10,12 @@ class Sound:
     def set_soundOn(self):
         print("소리 켜기 버튼 눌림")
 
+        global bgm_on
         bgm_on = True
 
     # 메뉴화면 환경설정 함수
     def set_soundOff(self):
         print("소리 끄기 버튼 눌림")
 
+        global bgm_on
         bgm_on = False
